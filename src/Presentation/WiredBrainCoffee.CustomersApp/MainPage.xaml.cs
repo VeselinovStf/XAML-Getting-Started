80aa6aedc56e1bb7a9ed8dc0c59c3ad1a700d288
veselinovStf <veselinovStf@gmail.com>
2020-11-01 11:25:22 +0200

    Starting Up app

--- src/Presentation/WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
+++ src/Presentation/WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +15,12 @@ namespace WiredBrainCoffee.CustomersApp
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void AddCustomerButton_ClickHandler(object sender, RoutedEventArgs e)
+        {
+            var messageDialog = new MessageDialog("Custommer Added");
+            await messageDialog.ShowAsync();
         }
     }
 }
