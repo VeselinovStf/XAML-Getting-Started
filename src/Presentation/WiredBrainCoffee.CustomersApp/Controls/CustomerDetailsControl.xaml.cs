00e4a92f731d4db4aba15da93fd7a5d26e4bed38
s v <chofexx@gmail.com>
2020-11-07 09:09:50 +0200

    Control Data Binding

--- src/Presentation/WiredBrainCoffee.CustomersApp/Controls/CustomerDetailsControl.xaml.cs
+++ src/Presentation/WiredBrainCoffee.CustomersApp/Controls/CustomerDetailsControl.xaml.cs
@@ -21,37 +21,8 @@ namespace WiredBrainCoffee.CustomersApp.Controls
         // Using a DependencyProperty as the backing store for Customer.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty CustomerProperty =
             DependencyProperty.Register("Customer", typeof(Customer), 
-                typeof(CustomerDetailsControl), new PropertyMetadata(null, CustomerChangeCallback));
+                typeof(CustomerDetailsControl), new PropertyMetadata(null));
 
-        private static void CustomerChangeCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (d is CustomerDetailsControl customerDetailsControl)
-            {
-                var customer = e.NewValue as Customer;
-                customerDetailsControl.firstNameText.Text = customer?.FirstName ?? "";
-                customerDetailsControl.lastNameText.Text = customer?.LastName ?? "";
-                customerDetailsControl.isDeveloperCheckBox.IsChecked = customer?.IsDeveloper;
-            }
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            UpdateCustomer();
-        }
-
-        private void CheckBoxIsDeveloper_Handler(object sender, RoutedEventArgs e)
-        {
-            UpdateCustomer();
-        }
-
-        private void UpdateCustomer()
-        {
-            if (Customer != null)
-            {
-                Customer.FirstName = firstNameText.Text;
-                Customer.LastName = lastNameText.Text;
-                Customer.IsDeveloper = isDeveloperCheckBox.IsChecked.GetValueOrDefault();
-            }
-        }
+     
     }
 }
